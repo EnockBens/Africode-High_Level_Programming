98f978ec766fc256885f1ac441fcbab7758335ec
Enock Bens <bensonkings81@gmail.com>
2024-05-15 10:28:22 +0300

    Boolean lesson

---
+++ Python/Exercise/booleans.py
@@ -0,0 +1,13 @@
+is_present = True
+is_raining = False
+
+# print(is_raining)
+# print(is_present)
+
+# if is_raining :
+#     print("take an umbrella")
+# else :
+#     print("no need for an umbrella")
+
+results = not is_raining and not is_present
+print(results)
