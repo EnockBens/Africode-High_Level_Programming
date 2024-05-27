4d6ac759a9192263f64aaae1fd7cef76043033d5
Enock Bens <bensonkings81@gmail.com>
2024-05-27 09:59:57 +0300

    addded a loops file

---
+++ Python/Exercise/loops.py
@@ -0,0 +1,145 @@
+# for num in range(1, 11):
+#     if num == 6:
+#         # print("number found")
+#         continue
+
+#     print(num)
+
+# x = 1
+# # while x <= 20:
+  
+# #     print(x)
+    
+# # #     x -= 50
+
+x = 1
+
+# while True:
+#     if x < 10000000000000000000000000:
+#      print(x * " . ")
+#      print(x)
+    
+#     x += 1
+# x = 1
+ 
+# for i in range (2,20,7):
+#     for x in "abcdef":
+#         print (i+1,x)
+
+
+
+
+
+
+
+# fruits = [1 , 2 ,3]
+
+# for fruit in fruits :
+#     print(fruit)
+
+
+# students = ["Naomi" , "Gladwel" , "Nicko" , "Bett" , "Benz"]
+# # for student in students :
+# for i , student in enumerate (students):
+#         print(i+1, student)
+
+
+
+# for (i) in range (5):
+    # print(i)
+# colours = ["red" , "green" , "red"]
+ 
+# for i in fruits:
+#     for y in "abc":
+#         print (i,y)
+
+
+
+
+# name = input( "Enter Your Name: ")
+
+
+
+# sum = 0
+
+# for i in range(4,20,2):
+#    if sum % 2 == 0:
+#     sum += i
+#    print(sum)
+
+
+# fruits = ["bannan" , "apple" , "orange" , "three tomatoes"]
+
+# # for fruit in fruits:
+# for i,  fruit in enumerate(fruits):
+#     print(i+1 ,fruit)
+#     if fruit == "orange":
+#         break
+#     print(fruit)
+
+
+# # x = 1
+
+# # while True
+
+# # if x < 
+
+
+
+# for i in range(2,20,2):
+#     print(i)
+
+# while True :
+#     print("i will keep running until you press 'crl' + 'c' in your terminal")
+
+
+# data_ = input("Enter anything or 'q'to quit: ")
+# while data_ != "Q" and data_ != "q":
+#     data_ = input("Enter anything or Q to quit: ")
+# print(f"You entered {data_}") 
+
+# data_ = input("Enter anything or 'q' to quit: ")
+# while data_ != "Q" and data_ != "q":
+#     print(f"You entered: {data_}")
+#     data_ = input("Enter anything or 'Q' to quit: ")
+# print(f"You entered: {data_}")
+
+
+
+
+# data_ = input("Enter anything or 'q' to quit: ")
+# while True :
+#     if data_ != "Q" and data_ != "q":
+#         print(f"You entered: {data_}")
+#         data_ = input("Enter anything or 'Q' to quit: ")
+
+#     elif data_ == "Q" or "q":
+#         print("You have quitted")
+#         break
+
+
+
+
+
+
+
+
+
+for i in range(20):
+    if i ==6:
+        continue
+    else:
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
